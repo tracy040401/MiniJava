3268b40d9fa4fdf658664e49be0b9cc8c2aec1cc
tracy040401 <tracy@DESKTOP-C7D2AVF>
2024-04-15 17:15:40 +0200

    Several instrcutions in main added

---
+++ test/good/TestMainServeral.java
@@ -0,0 +1,9 @@
+class TestMainServeral {
+    public static void main(String[] args){ 
+        if(2<=1) System.out.println(42);
+        else System.out.println(0);
+
+        if(2>1) System.out.println(0);
+        else System.out.println(3);
+    }
+}
